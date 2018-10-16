1e67cc4bca0b19adf36709b576a6f51cc5a441f7
SpencerNorris <spencercnorris2017@gmail.com>
2018-10-16 03:13:07 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ conda install pytorch-cpu torchvision-cpu -c pytorch
 
 mkdir -p /storage/s8zrzug3/models/gan/
 mkdir -p /storage/s8zrzug3/datasets/fashion-mnist/
+mkdir -p /storage/s8zrzug3/results/gan/
 
 # train
-python main.py --gan_type BEGAN --dataset fashion-mnist --epoch 20 --batch_size 64 -s
+python main.py --gan_type BEGAN --dataset fashion-mnist --epoch 20 --batch_size 64 --result-dir /storage/s8zrzug3/results/gan/ -s
