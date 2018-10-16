fedd9a2d82fa80591fe5eeb9450241ba4fcbb1b1
SpencerNorris <spencercnorris2017@gmail.com>
2018-10-16 03:14:38 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ mkdir -p /storage/s8zrzug3/datasets/fashion-mnist/
 mkdir -p /storage/s8zrzug3/results/gan/
 
 # train
-python main.py --gan_type BEGAN --dataset fashion-mnist --epoch 20 --batch_size 64 --result-dir /storage/s8zrzug3/results/gan/ -s
+python main.py --gan_type BEGAN --dataset fashion-mnist --epoch 20 --batch_size 64 --result_dir /storage/s8zrzug3/results/gan/ -s
